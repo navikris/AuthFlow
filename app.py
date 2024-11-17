72017873e3da9ab09a6598423f2e7b880e56493c
unfair <navi.kris2001@gmail.com>
2024-11-17 15:00:19 +0530

    Cleanup

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, redirect, url_for, session, abort, request
-import requests
+from flask import Flask, redirect, url_for, session, abort
 from datetime import timedelta
 from authlib.integrations.flask_client import OAuth
 from functools import wraps
@@ -18,7 +17,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=9999)
 
 # oauth config
 oauth = OAuth(app)
-huggingface = oauth.register(
+oauth.register(
     name='huggingface',
     client_id='4b0904ee-f186-4259-9878-3d082fd57e20',
     client_secret='3b29d70f-7566-468d-beb0-d7065159d16a',
@@ -38,34 +37,6 @@ def login_required(f):
         # You would add a check here and usethe user id or something to fetchs
         # the other data for that user/check if they exist
         if user:
-            # try:
-            #     client = InferenceClient(
-            #         model = "NousResearch/Hermes-3-Llama-3.1-8B",
-            #         # token = "hf_1asduioaiusdiu",
-            #         token = dict(session)['user_token']['access_token'],
-            #         timeout = 60.0,
-            #     )
-            # except:
-            # print(f"token={session["user_token"]}")
-            # huggingface = oauth.create_client('huggingface')  # create the hugginface oauth client
-            # # if not huggingface:
-            # #     abort(404)
-            # # print(dir(huggingface))
-            # # print(dir(oauth))
-            # new_token = huggingface.fetch_access_token(
-            #     token_url='https://huggingface.co/oauth/token',
-            #     refresh_token=session['user_token']['refresh_token'],
-            #     grant_type='refresh_token',
-            #     scope='https://huggingface.co'
-            # )
-
-            # huggingface = oauth.create_client('huggingface')  # create the hugginface oauth client
-            # if not huggingface:
-            #     abort(404)
-            # token = huggingface.authorize_access_token(grant_type='refresh_token', refresh_token=session['user_token']['refresh_token'])  # Access token from hugginface (needed to get user info)
-            # token = huggingface.authorize_access_token()  # Access token from hugginface (needed to get user info)
-            # session['user_token'].update(token)
-            # print(f"token={session["user_token"]}")
             if int(time.time()) > session["user_token"]["expires_at"] - 5:
                 return redirect(url_for("login"))
             else:
@@ -78,68 +49,32 @@ def login_required(f):
 @login_required
 def hello_world():
     numbers = []
-    for _ in range(4): # testing for actual token
-        client = InferenceClient(
-            model = "NousResearch/Hermes-3-Llama-3.1-8B",
-            # token = "hf_1asduioaiusdiu",
-            token = dict(session)['user_token']['access_token'],
-            timeout = 60.0,
-        )
-        agent_reply = client.chat_completion(
-            messages=[{"role": "user", "content": f"give me a random number between {random.randint(0, 50)} and {random.randint(50, 100)}, dont say anything else"}],
-            max_tokens=200,
-            stream=False,
-        )
-        numbers.append(agent_reply.choices[0].message.content)
-    # uername = dict(session)['profile']
-    return f'Hello, here are your numbers: {numbers}!'
+    client = InferenceClient(
+        model = "NousResearch/Hermes-3-Llama-3.1-8B",
+        token = dict(session)['user_token']['access_token'],
+        timeout = 60.0,
+    )
+    agent_reply = client.chat_completion(
+        messages=[{"role": "user", "content": f"give me a random number between {random.randint(0, 50)} and {random.randint(50, 100)}, dont say anything else"}],
+        max_tokens=200,
+        stream=False,
+    )
+    return f'Hello, here is your number: {agent_reply.choices[0].message.content}!'
 
 
 @app.route('/login')
 def login():
-    huggingface = oauth.create_client('huggingface')  # create the hugginface oauth client
-    if not huggingface:
-        abort(404)
     redirect_uri = url_for('authorize', _external=True)
-    return huggingface.authorize_redirect(redirect_uri)
+    return oauth.huggingface.authorize_redirect(redirect_uri)
 
 
 @app.route('/authorize')
 def authorize():
-    print("##################333")
-    print(request.data)
-    huggingface = oauth.create_client('huggingface')  # create the hugginface oauth client
-    if not huggingface:
-        abort(404)
-    token = huggingface.authorize_access_token()  # Access token from hugginface (needed to get user info)
-    print(f"{token=}")
+    token = oauth.huggingface.authorize_access_token()  # Access token from hugginface (needed to get user info)
     user = oauth.huggingface.userinfo()  # uses openid endpoint to fetch user info
-    print(f"{user=}")
     session['user_token'] = token
     session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
     return redirect('/')
-
-
-@app.route('/revoke_token')
-def revoke_token():
-    token = session.get('user_token')
-    if token:
-        token_url = 'https://huggingface.co/oauth/revoke'
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        data = {
-            'token': token['access_token'],
-            'token_type_hint': 'access_token',
-            'client_id': '4b0904ee-f186-4259-9878-3d082fd57e20',
-            'client_secret': '3b29d70f-7566-468d-beb0-d7065159d16a'
-        }
-        response = requests.post(token_url, headers=headers, data=data)
-        print(response)
-        if response.status_code == 200:
-            session.pop('user_token', None)
-            return 'Token revoked successfully!'
-        else:
-            return 'Error revoking token', 400
-    return 'No token to revoke', 400
 
 
 @app.route('/logout')
